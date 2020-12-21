4319e4aa60ec30a6f61c9a26c9720285913fb58a
hello_sarah <shenruhui@outlook.com>
2020-12-21 17:04:32 +0800

    Update MCTS.py

--- simple_chess/MCTS.py
+++ simple_chess/MCTS.py
@@ -33,7 +33,7 @@ class MCTSAgent(object):
 
         self.simulation_times = 0 # the times of simulation
         self.begin_time = time.time() # start the MCTS time
-        self.time_limit = 9
+        self.time_limit = 6
         self.temperature = 0.5
 
 
